e47a0640c91e48e7327f035041a4be7f071b43ba
kalyani adurthi <kalyaniadurthi99@gmail.com>
2020-11-13 09:15:01 +0530

    modifid -one email can be registered one time

--- register.js
+++ register.js
@@ -7,8 +7,19 @@ function register()
            var password = document.getElementById("password").value;     
            
        var userObj={"username":Name, "email":email, "password":password };
+       var usersr=JSON.parse(localStorage.getItem("USERS")) || [];
+       if(!usersr.some(user=>user.email===userObj.email)){
+        usersr.push(userObj);
+        localStorage.setItem("USERS",JSON.stringify(usersr));
+           location.reload();
+          console.log(userObj);
+          alert("continue login ");
+    }
+    else{
+        alert("user already exists try registering with different credentials");
+    }
        var registerserviceObj= new registerservice();
        registerserviceObj.register(userObj);
-       alert("registered successfully");
-       window.location.href="login.html";
        }
+
+      
